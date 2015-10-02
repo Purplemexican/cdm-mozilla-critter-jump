bfd3d46bfef367b082a69f47b1a3cce2e6fba491
purplemexican <laststandcityunion@gmail.com>
2015-10-02 22:24:55 +0000

    add restart and menu icon to preloader

--- js/Preloader.js
+++ js/Preloader.js
@@ -42,6 +42,8 @@ Critterer.Preloader.prototype = {
         this.load.image('background', 'img/background.png');
         this.load.image('btnPause', 'img/hud/pause.png');
 	    this.load.image('panel', 'img/hud/pause-modal.png');
+	    this.load.image('restart', 'img/hud/restart-icon.png');
+	    this.load.image('menu', 'img/hud/menu-icon.png');
     },
 
     /**
